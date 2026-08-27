53e6b73b646af99463c08bc2b1a9be280277fa30
Lalit katre <126580463+LalitKatre4@users.noreply.github.com>
2026-08-27 19:52:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    
-  ],
-  base: '/protoolkit/', // <-- यह लाइन जोड़ें (अपनी repo का नाम)
+  plugins: [react()],
+  base: '/protoolkit/', // Dono taraf slash zaroor lagayein
 })
